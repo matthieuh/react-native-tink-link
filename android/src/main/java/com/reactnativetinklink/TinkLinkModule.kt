ca1d8d62667d5e9c8fa6f15b0ad6f4ee5e512dd0
Erik Dreifaldt <erik.dreifaldt@gmail.com>
2021-11-02 13:27:32 +0100

    add AndroidThreeTen to android

--- android/src/main/java/com/reactnativetinklink/TinkLinkModule.kt
+++ android/src/main/java/com/reactnativetinklink/TinkLinkModule.kt
@@ -1,29 +1,31 @@
 package com.reactnativetinklink
 
+import android.util.Log
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.jakewharton.threetenabp.AndroidThreeTen
 
 class TinkLinkModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName(): String {
-        return "TinkLink"
-    }
+  override fun getName(): String {
+    return "TinkLink"
+  }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
-    }
+  init {
+    AndroidThreeTen.init(reactContext);
+  }
 
-    @ReactMethod
-    fun displayTinkLink(a: String, b: String, promise: Promise) {
-      promise.resolve(a + b)
-    }
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun multiply(a: Int, b: Int, promise: Promise) {
+    promise.resolve(a * b)
+  }
 
-    
+  @ReactMethod
+  fun displayTinkLink(a: String, b: String, promise: Promise) {
+    promise.resolve(a + b)
+  }
 }
